33ddb3f78a15fada8f55724d191a5a8e8ade0169
Michael Janiak <michael.janiak@gmail.com>
2013-09-12 14:16:03 -0400

    Site updated at 2013-09-12 18:16:03 UTC

--- javascripts/google_form.js
+++ javascripts/google_form.js
@@ -1,67 +1,3 @@
-// Accommodate running jQuery or Zepto in noConflict() mode by
-// using an anonymous function to redefine the $ shorthand name.
-// See http://docs.jquery.com/Using_jQuery_with_Other_Libraries
-// and http://zeptojs.com/
-var libFuncName = null;
-
-if (typeof jQuery === "undefined" &&
-    typeof Zepto === "undefined" &&
-    typeof $ === "function") {
-  libFuncName = $;
-} else if (typeof jQuery === "function") {
-  libFuncName = jQuery;
-} else if (typeof Zepto === "function") {
-  libFuncName = Zepto;
-} else {
-  throw new TypeError();
-}
-
-(function ($, window, document, undefined) {
-  //clean up empty tags
-  $('.google-form-wrapper p, .google-form-wrapper br')
-    .filter(function() {
-      return $(this).html() == '';
-    })
-    .remove();
-  
-  //add required class to required elements
-  $('.google-form-wrapper form')
-    .find('.ss-item-required input, .ss-item-required textarea')
-    .filter(function() {
-      return $(this).attr('name').match(/entry\.\d\.single/);
-    })
-    .addClass('required');
- 
-  //validate the form
-/*  
-  $('.google-form-wrapper form').validate({
-    submitHandler: function(form) {
-      $(form)
-        .ajaxSubmit({
-          success: function(data) {
-            if (data) {
-              $(form)
-              .hide(200, function() {
-                $(this)
-                  .prev('.success-msg')
-                  .fadeIn('slow')
-              })
-            }
-          },
-          error : function (data) {
-            console.error(data);
-          }
-        })
-    }
-  });
-  */
-  
-  
-}(libFuncName, this, this.document));
-
-
-
-/*
 jQuery(document).ready(function() {
   //clean up empty tags
   jQuery('.google-form-wrapper p, .google-form-wrapper br')
@@ -100,4 +36,3 @@ jQuery(document).ready(function() {
     }
   });
 });
-*/
